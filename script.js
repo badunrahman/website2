c489f9c8a9cfce0b237e28d8003ab6f341b9724c
badunrahman <123969525+badunrahman@users.noreply.github.com>
2024-05-01 19:56:11 -0400

    website demo one

---
+++ script.js
@@ -0,0 +1,8 @@
+/* this is for the toggle menu and */
+
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
